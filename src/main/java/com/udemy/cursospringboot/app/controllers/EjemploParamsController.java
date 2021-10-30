f6063ff3a06964a578943046cdc3b2a5e910f342
vic-cross <vic.crossgr@gmail.com>
2021-10-30 15:39:06 -0500

    ruta con multiples parametros

--- src/main/java/com/udemy/cursospringboot/app/controllers/EjemploParamsController.java
+++ src/main/java/com/udemy/cursospringboot/app/controllers/EjemploParamsController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/params")
 public class EjemploParamsController {
@@ -14,10 +16,34 @@ public class EjemploParamsController {
     public String index(){
         return "params/index";
     }
+
     @GetMapping("/string")
     public String param(@RequestParam(name = "text", required = false, defaultValue = "algun valor...") String text, Model model){
         //la anotacion permite asignar el nombre que utilizara en la peticion y si es requerido
         String resultado = "El texto enviado es: " + text;
+        model.addAttribute("resultado", resultado);
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params")
+    public String mixParam(@RequestParam String saludo , @RequestParam Integer numeroVeces, Model model){
+        //si se omite el nombre queda igual en la peticion
+        String resultado = "El saludo enviado es: '" + saludo + "' y el numero enviado es: '" + numeroVeces + "'";
+        model.addAttribute("resultado", resultado);
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params-request")
+    public String mixParam(HttpServletRequest request, Model model){
+        //los parametros se pueden obtener a traves del objeto request
+        int numeroVeces;
+        String saludo = request.getParameter("saludo");
+        try {
+            numeroVeces = Integer.parseInt(request.getParameter("numeroVeces"));
+        } catch (NumberFormatException ex){
+            numeroVeces = 0;
+        }
+        String resultado = "El saludo enviado es: '" + saludo + "' y el numero enviado es: '" + numeroVeces + "'";
         model.addAttribute("resultado", resultado);
         return "params/ver";
     }
